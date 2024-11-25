a4e8605d7baa1c5b63981a7e0c51d66afbf60425
Rob Hess <hessro@oregonstate.edu>
2024-11-25 15:34:00 -0800

    Add code from 11/25 lecture.

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express')
+var exphbs = require('express-handlebars')
+
+var peopleData = require("./peopleData.json")
+console.log("== peopleData:", peopleData)
 
 var port = process.env.PORT || 8000
 var app = express()
+
+app.engine("handlebars", exphbs.engine({
+  defaultLayout: "main"
+}))
+app.set("view engine", "handlebars")
 
 app.use(express.static('static'))
 
@@ -9,20 +18,17 @@ app.get('/people', function (req, res, next) {
   res.status(200).sendFile(__dirname + '/static/people.html')
 })
 
-var availablePeople = [
-  'luke',
-  'leia',
-  'rey',
-  'finn',
-  'r2d2'
-]
 
 app.get('/people/:person', function (req, res, next) {
   var person = req.params.person.toLowerCase()
-  if (availablePeople.indexOf(person) >= 0) {
-    res.status(200).sendFile(
-      __dirname + '/static/people/' + person + '.html'
-    )
+  console.log("== person:", person)
+  var personData = peopleData[person]
+  if (personData) {
+    res.render("photoPage", {
+      name: personData.name,
+      photos: personData.photos,
+      renderGreeting: true
+    })
   } else {
     next()
   }
